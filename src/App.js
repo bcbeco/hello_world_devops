13eb87feddf40d71b226d6a1923a1ca55ff08f80
Berencsi Béla <Rendszeradmin@SPECIALTERV.HU>
2025-12-03 15:38:38 +0100

    Lábléc hozzáadva

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <h1>Hello World DevOps! 🚀</h1>
       <p>Ez az első verzió</p>
       <p><strong>Version: 1.0.0</strong></p>
+      <footer style={{ marginTop: '100px', color: '#666' }}>
+        Made with DevOps practices
+      </footer>
     </div>
   );
 }
